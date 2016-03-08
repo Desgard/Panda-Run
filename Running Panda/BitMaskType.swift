ea3314dbbb564f9ef20ac6a0720ce8f40cb6e5b8
dgytdhy <desgard_duan@foxmail.com>
2016-03-08 16:53:57 +0800

    first commit

---
+++ Running Panda/BitMaskType.swift
@@ -0,0 +1,9 @@
+//
+//  BitMaskType.swift
+//  Running Panda
+//
+//  Created by 段昊宇 on 16/3/8.
+//  Copyright © 2016年 Desgard_Duan. All rights reserved.
+//
+
+import Foundation
---
+++ Running Panda/Platform.swift
@@ -0,0 +1,9 @@
+//
+//  Platform.swift
+//  Running Panda
+//
+//  Created by 段昊宇 on 16/3/7.
+//  Copyright © 2016年 Desgard_Duan. All rights reserved.
+//
+
+import Foundation
